8bf50a4a68dcd405d0f3d02ab2ad83cdf3412a6a
Humayun Kabir <humayunkabir756@gmail.com>
2025-08-29 12:31:39 +0600

    copy button work done

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-const heartBtns = document.getElementsByClassName('heart_btn');
+const heartBtns = document.getElementsByClassName("heart_btn");
 
-for(let heartBtn of heartBtns){
-  heartBtn.addEventListener('click' , function(){
-    const heartCount = document.getElementById('heartCount').innerText;
+for (let heartBtn of heartBtns) {
+  heartBtn.addEventListener("click", function () {
+    const heartCount = document.getElementById("heartCount").innerText;
     const currentCount = Number(heartCount) + 1;
 
-   document.getElementById('heartCount').innerText = currentCount;
-      
-  })
+    document.getElementById("heartCount").innerText = currentCount;
+  });
 }
 
-// =============================================================
+// ============================= Heart Work Done ================================
+// ========================== Emergency Call work Start ==============================
 
 let coins = 100;
-const coinElement = document.querySelector(".coinCount")
-const coinCount = document.querySelector("coinCount");
+
+const coinCount = document.querySelector(".coinCount");
 const callBtns = document.getElementsByClassName("call_btn");
 const cartContainer = document.getElementById("cart_container");
 
@@ -23,18 +23,18 @@ for (let callBtn of callBtns) {
     const callerTitle = callBtn.parentNode.parentNode.children[1].innerText;
     const callerNumber = callBtn.parentNode.parentNode.children[3].innerText;
 
-      if (coins < 20) {
+    if (coins < 20) {
       alert("Call করার জন্য আপনার কাছে অন্তত ২০ কয়েন থাকতে হবে!");
       return;
     }
 
-     coins -= 20;
-    coinElement.innerText = coins;
+    coins -= 20;
+    coinCount.innerText = coins;
 
     alert(`${callerTitle} কল হচ্ছে...  নাম্বার: ${callerNumber}`);
-   
+
     const newCart = document.createElement("div");
-    const date =new Date().toLocaleTimeString()
+    const date = new Date().toLocaleTimeString();
 
     newCart.innerHTML = `
              <div class="bg-gray-100 rounded-xl py-1 px-2 mt-3 flex justify-between items-center">
@@ -46,10 +46,33 @@ for (let callBtn of callBtns) {
                 </div>
         `;
 
-        cartContainer.append(newCart)
+    cartContainer.append(newCart);
   });
 }
-document.getElementById('clearBtn').addEventListener('click', function(){
-    const cartContainer = document.getElementById("cart_container");
-    cartContainer.innerHTML = "";
-})
+document.getElementById("clearBtn").addEventListener("click", function () {
+  const cartContainer = document.getElementById("cart_container");
+  cartContainer.innerHTML = "";
+});
+
+// ========================== Emergency Call work Done ==============================
+// ========================== Copy Button work Start ==============================
+
+let copyCount = 0;
+const copyCounter = document.querySelector(".copy_count");
+const copyButtons = document.getElementsByClassName("copy_btn");
+
+for (const copyBtn of copyButtons) {
+  copyBtn.addEventListener("click", function () {
+    const callerNumber = copyBtn.parentNode.parentNode.children[3].innerText;
+
+    navigator.clipboard.writeText(callerNumber).then(() => {
+      alert(`নম্বর কপি হয়েসে: ${callerNumber}`);
+
+      copyCount++;
+      copyCounter.innerText = copyCount;
+    });
+  });
+}
+// ========================== Copy Button work Done ==============================
+
+
