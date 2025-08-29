4ca755a42b801a81b5bb54448fe65b5fafa52037
Humayun Kabir <humayunkabir756@gmail.com>
2025-08-29 11:09:48 +0600

    call Button JS Done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-// Heart Count
-// const heartCount = document.getElementById("heartCount");
-// const heartBtn = document.querySelectorAll(".fa-heart");
-
-// for (let i = 0; i < heartBtn.length; i++) {
-//   heartBtn[i].addEventListener("click", function () {
-//     heartCount.innerText = Number(heartCount.innerText) + 1;
-//   });
-// }
 const heartBtns = document.getElementsByClassName('heart_btn');
 
 for(let heartBtn of heartBtns){
@@ -18,3 +9,47 @@ for(let heartBtn of heartBtns){
       
   })
 }
+
+// =============================================================
+
+let coins = 100;
+const coinElement = document.querySelector(".coinCount")
+const coinCount = document.querySelector("coinCount");
+const callBtns = document.getElementsByClassName("call_btn");
+const cartContainer = document.getElementById("cart_container");
+
+for (let callBtn of callBtns) {
+  callBtn.addEventListener("click", function () {
+    const callerTitle = callBtn.parentNode.parentNode.children[1].innerText;
+    const callerNumber = callBtn.parentNode.parentNode.children[3].innerText;
+
+      if (coins < 20) {
+      alert("Call করার জন্য আপনার কাছে অন্তত ২০ কয়েন থাকতে হবে!");
+      return;
+    }
+
+     coins -= 20;
+    coinElement.innerText = coins;
+
+    alert(`${callerTitle} কল হচ্ছে...  নাম্বার: ${callerNumber}`);
+   
+    const newCart = document.createElement("div");
+    const date =new Date().toLocaleTimeString()
+
+    newCart.innerHTML = `
+             <div class="bg-gray-100 rounded-xl py-1 px-2 mt-3 flex justify-between items-center">
+                    <div class="">
+                  <h2 class="font-regular">${callerTitle}</h2>
+                  <h3>${callerNumber}</h3>
+                </div>
+                  <p>${date}</p>
+                </div>
+        `;
+
+        cartContainer.append(newCart)
+  });
+}
+document.getElementById('clearBtn').addEventListener('click', function(){
+    const cartContainer = document.getElementById("cart_container");
+    cartContainer.innerHTML = "";
+})
